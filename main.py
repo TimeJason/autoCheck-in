ad57f88637ff857ff6f3e233bda5bc02f4006178
TimeJason <105142157+TimeJason@users.noreply.github.com>
2023-08-20 21:14:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from pydantic import BaseModel
 
 class Push(BaseModel):
     # 是否开启推送 默认关闭
-    PUSH_OR_NOR: bool = False
+    PUSH_OR_NOR: bool = True
     # PUSHPLUS的TOKEN
     PUSHPLUS_TOKEN: str = None
 
